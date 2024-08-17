78439466332f4b2ae04890f434ab1c7f40a5bd3a
Bruno Coiffier <149143143+gbe668@users.noreply.github.com>
2024-08-17 09:56:25 +0200

    Add files via upload

---
+++ appli/clubs/conf_SERVER_NAME.php
@@ -0,0 +1,8 @@
+<?php
+// Define database configuration
+define("DB_HOST", "dbhost");
+define("DB_USER", "dbuser");
+define("DB_PASS", "dbpass");
+define("DB_NAME", "dbname");
+define("DB_PORT", 3306 );
+?>
